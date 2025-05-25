a9727f2bb54e6f8614112a2aa15770222c9ee829
Gabriel Nkrumah-Antiedu <gabdevnkrumah@gmail.com>
2025-05-25 19:39:13 +0200

    Print 'C is fun' x times with proper validation and two console.logs

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,3 +1,2 @@
 const x = parseInt(process.argv[2]);
-if (!isNaN(x) && x > 0) for (let i = 0; i < x; i++) console.log("C is fun");
-else console.log("Missing number of occurrences");
+isNaN(x) || x <= 0 ? console.log("Missing number of occurrences") : (() => { for (let i = 0; i < x; i++) console.log("C is fun"); })();
