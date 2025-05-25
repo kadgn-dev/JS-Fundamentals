735f8a584cacefa88cfb6ffba25541f9fad2fbca
Gabriel Nkrumah-Antiedu <gabdevnkrumah@gmail.com>
2025-05-25 19:00:26 +0200

    Add 2-arguments.js script to print message based on number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.length - 2; // Subtract 2 to exclude 'node' and the script name
+
+if (args === 0) {
+    console.log("No argument");
+} else if (args === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
